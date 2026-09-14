d9dc0a8de33c3a539fefd18e58e1647eadac6bad
Hanna <hanna.svender1@gmail.com>
2026-09-14 16:13:32 +0200

    Add application endpoints

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # beskriver var databasen finns och hur den ansluts
 DATABASE_URL = "postgresql://hanna@localhost/release_tracker"
@@ -7,5 +7,15 @@ DATABASE_URL = "postgresql://hanna@localhost/release_tracker"
 # sqlalchemy engine hanterar kommunikationen mellan kod och db
 engine = create_engine(DATABASE_URL)
 
+# arbetsyta för skriva/läsa data
+SessionLocal = sessionmaker(bind=engine)
+
 # utgångspunkt för db-modell
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from backend.database import Base, engine
+from backend.database import Base, engine, get_db
 from backend import models
+from backend.models import Application
+
+from backend.schemas import ApplicationCreate
+
 
 # titta på alla modeller kopplade till Base -> skapa tabeller som saknas i db
 Base.metadata.create_all(bind=engine)
@@ -11,3 +16,24 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Release Deployment Tracker API"}
+
+@app.get("/api/applications")
+def get_applications(db: Session = Depends(get_db)):    # hämta db-session när endpoint körs
+    return db.query(Application).all()  # hämta alla rader fr applications
+
+@app.post("/api/applications")
+def create_application(
+    application: ApplicationCreate,
+    db: Session = Depends(get_db)
+
+):
+    new_application = Application(
+        name=application.name,
+        description=application.description
+    )
+
+    db.add(new_application)
+    db.commit()
+    db.refresh(new_application) # hämtar tillbaka sparade posten från db -> får det automatiskt skapade id-värdet
+
+    return new_application
